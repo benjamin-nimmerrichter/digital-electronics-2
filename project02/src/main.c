af28925cd66dde0947591c92255435b5bef97d4c
benjamin-nimmerrichter <99691021+benjamin-nimmerrichter@users.noreply.github.com>
2022-12-14 10:10:43 +0100

    Add files via upload

--- project02/src/main.c
+++ project02/src/main.c
@@ -1,36 +1,32 @@
-#ifndef F_CPU
-#define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
-#endif
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>     // AVR device-specific IO definitions
 //#include <util/delay.h> // Functions for busy-wait delay loops
 #include <timer.h> 
 //#include <uart.h>
-#include <gpio.h>
 #include <avr/interrupt.h>
-#include <stdlib.h> 
+ 
 
 // -----
 // This part is needed to use Arduino functions but also physical pin
 // names. We are using Arduino-style just to simplify the first lab.
-#define OFFSET 128
-#define CONST 0.70556
-uint8_t duty0 = 128;
-uint8_t duty1 = 128;
-uint8_t degrees = 0;
-int8_t i;
-int8_t ovfs = 0;
+#define OFFSET 32 // minimum for 0,9923ms PW
+#define CONST 0.35f // constant 63/180=0,35
+uint8_t duty = 128;
+short degrees0 = 0;
+short degrees1 = 0;
 
+short i = 0;
+short j = 0;
 
 int main(void)
 {
   SREG |= (1<<7);
   //set WGM to fast PWM (mode 7)
-  TCCR1B |= (1<<CS11) | (1<<CS10);
+  TCCR1B |= (1<<CS12);
   //duty cycle
-  OCR1A = 0x80;
-  OCR1B = 0x20;
+  //OCR1A = 0x3F; //48+15=63
+  //OCR1B = 0x20; //32
   //set COM0A and COM0B to 1 for fast PWM output
   TCCR1A |= (1<<COM1A1) | (1<<COM1B1) | (1<<WGM10) | (1<<WGM11)| (1<<WGM12); TCCR1A &=~((1<<COM1A0) | (1<<COM1B0) | (1<<WGM13));
   //OCR0B = 0x80;
@@ -48,26 +44,30 @@ int main(void)
 
 ISR(TIMER0_OVF_vect)
 {
-  if(ovfs >= 5)
+  
+  if (degrees0>=120)
   {
-  if (degrees>=127)
+    i = 5;
+  }
+  else if (degrees0==0)
   {
-    i = -1;
+    i = -5;
   }
-  if (degrees==0)
+   degrees0 = degrees0+i;
+   duty = (uint8_t)((float)OFFSET+(CONST*((float)degrees0)));
+  OCR1A = duty;
+
+  if (degrees1>=120)
   {
-    i = +1;
+    j = 3;
+  }
+  else if (degrees1==0)
+  {
+    j = -3;
   }
 
-
-  duty0 = (uint8_t)(((double)OFFSET)+(CONST*((double)degrees)));
-  OCR1A = degrees;
-  OCR1B = 127-degrees;
-
-  degrees = degrees+i;
-  ovfs=0;
-  }
-  ovfs++;
-
+   degrees1 = degrees1+j; 
+  duty = (uint8_t)((float)OFFSET+(CONST*((float)degrees1)));
+  OCR1B = duty;
 }
  
